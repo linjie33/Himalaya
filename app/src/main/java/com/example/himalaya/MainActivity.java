c428dc4a2af1661549637db61596b8328c1c06a0
yunuser1929 <linjie_1997@outlook.com>
2019-06-04 17:40:55 +0800

    commit himalaya

--- app/src/main/java/com/example/himalaya/MainActivity.java
+++ app/src/main/java/com/example/himalaya/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.himalaya;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.himalaya.utils.LogUtil;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Map<String, String> map = new HashMap<>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(@Nullable CategoryList categoryList) {
+                List<Category> categories= categoryList.getCategories();
+                if(categories != null){
+                    int size = categories.size();
+                    Log.d(TAG,"categories size is <"+size);
+                    for(Category category : categories){
+                        //Log.d(TAG,"category -- >"+category.getCategoryName());
+                        LogUtil.d(TAG,"category -- >"+category.getCategoryName() );
+                    }
+
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                //Log.e(TAG,"error code --"+ i +"error msg == >"+s);
+                LogUtil.d(TAG,"error code --"+ i +"error msg == >"+s);
+            }
+        });
     }
 }
